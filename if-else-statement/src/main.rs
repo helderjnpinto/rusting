b4ad9bc8b8b02d9bc894ff44169192578c9e833b
hp <helder@yarilabs.com>
2023-02-10 13:06:03 +0000

    ifelse

---
+++ if-else-statement/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let n = 45;
+
+    if n < 10 {
+        println!("Rust!");
+    } else if n > 50 {
+        println!("Else if Rust!");
+    } else {
+        println!("Else Rust!");
+    }
+}
